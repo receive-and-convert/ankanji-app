0e8fe5658726f9db69f5a6102e0360c1f8c245c5
Veren Valensia <72638950+knownasvv@users.noreply.github.com>
2025-03-26 00:07:16 +0700

    Add previous card button

--- app/src/main/java/com/receiveandconvert/ankanji/component/card/FlashCard.kt
+++ app/src/main/java/com/receiveandconvert/ankanji/component/card/FlashCard.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.FloatingActionButton
@@ -64,6 +65,35 @@ fun FlashCard(cards: List<Card>, modifier: Modifier = Modifier) {
 			)
 		}
 
+		// Previous card button
+		FloatingActionButton(
+			onClick = {
+				if (!isAnimating) { // Check if an animation is already running
+					scope.launch {
+						isAnimating = true // Set the flag to true when animation starts
+						offsetX.animateTo(
+							targetValue = -1000f, // Slide to the left
+							animationSpec = tween(durationMillis = 150)
+						)
+						activeCard = shuffledCards[(shuffledCards.indexOf(activeCard) - 1 + shuffledCards.size) % shuffledCards.size]
+						isFlipped = false
+						offsetX.snapTo(1000f) // Move to the right off-screen
+						offsetX.animateTo(
+							targetValue = 0f, // Slide back to the center
+							animationSpec = tween(durationMillis = 150)
+						)
+						isAnimating = false // Reset the flag when animation ends
+					}
+				}
+			},
+			containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+			elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+			modifier = Modifier.align(Alignment.BottomStart)
+		) {
+			Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "PREVIOUS")
+		}
+
+		// Next card button
 		FloatingActionButton(
 			onClick = {
 				if (!isAnimating) { // Check if an animation is already running
