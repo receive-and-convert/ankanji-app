1db364b08d851916b80329804d23a12767bdc8c6
Veren Valensia <72638950+knownasvv@users.noreply.github.com>
2025-03-25 22:54:13 +0700

    Fix filter in lists

--- app/src/main/java/com/receiveandconvert/ankanji/model/VocabularyCard.kt
+++
@@ -1,8 +0,0 @@
-package com.receiveandconvert.ankanji.model
-
-data class VocabularyCard(
-	override val kanji: String,
-	override val kana: String,
-	override val translation: String,
-	val type: String
-): Card(kanji, kana, translation)
